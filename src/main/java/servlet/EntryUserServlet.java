a5d1352585ad9c87e10c558b6989f3a3b51a6e49
zamma ichiro <zamma ichiro@DESKTOP-BV5AC3O>
2025-01-14 17:49:43 +0900

    2回目のコミット

--- src/main/java/servlet/EntryUserServlet.java
+++ src/main/java/servlet/EntryUserServlet.java
@@ -1,6 +1,7 @@
 package servlet;
 
 import java.io.IOException;
+import java.util.regex.Pattern;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -15,51 +16,61 @@ import dao.Dao;
 //SampleBBS/EntryUserServletにアクセスすると動作
 @WebServlet("/EntryUserServlet")
 public class EntryUserServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-
+    private static final long serialVersionUID = 1L;
 
     public EntryUserServlet() {
         super();
     }
 
+    // パスワードが強化基準を満たしているかチェックするメソッド
+    private boolean isPasswordStrong(String password) {
+        // パスワードが8文字以上でなければならない
+        if (password.length() < 8) {
+            return false;
+        }
 
-    //POSTアクセス時に動作．データ登録系の処理なので，POSTのみ受け付け．
-    //
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		//requestのデータの文字コードをUTF-8に設定
-		request.setCharacterEncoding("UTF-8");
-		
-		//requestから，id,name,password1,password2の名前のデータを取得(entryUser.jspを参照)
-		String id = request.getParameter("id");
-		String name = request.getParameter("name");
-		String password1 = request.getParameter("password1");
-		String password2 = request.getParameter("password2");
+        // 正規表現: 小文字、大文字、数字、記号が含まれているかを確認
+        String regex = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$%^&*(),.?\":{}|<>]).+$";
+        Pattern pattern = Pattern.compile(regex);
+        return pattern.matcher(password).matches();
+    }
 
-		//DBアクセスのためのクラスをインスタンス化
-		Dao dao = new Dao();
+    // POSTアクセス時に動作．データ登録系の処理なので，POSTのみ受け付け．
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // requestのデータの文字コードをUTF-8に設定
+        request.setCharacterEncoding("UTF-8");
 
-		//ユーザ情報が問題ないかチェック
-		if(		!id.isBlank() //idが空もしくは空白のみではない
-				&& dao.getUserById(id)==null //idが使用されていない
-				&& password1.equals(password2) //確認用パスワードが一致
-				&& !password1.isBlank() //passwordが空もしくは空白のみではない
-				) {
-			
-			//登録用のユーザオブジェクトを作成．Userはbeansパッケージに定義してある．
-			User userToEntry = new User(id, password1, name);
-			//ユーザをDBに登録
-			dao.insertUser(userToEntry);
-			//ログインページ./WEB-INF/jsp/login.jspに移動
-			RequestDispatcher dispatcher = request.getRequestDispatcher("./WEB-INF/jsp/login.jsp");
-			dispatcher.forward(request, response);
-		}
-		else {//ユーザ情報に問題があった場合
-			//ユーザ登録ページに戻す．
-			RequestDispatcher dispatcher = request.getRequestDispatcher("./WEB-INF/jsp/entryUser.jsp");
-			dispatcher.forward(request, response);
-		}
+        // requestから，id,name,password1,password2の名前のデータを取得
+        String id = request.getParameter("id");
+        String name = request.getParameter("name");
+        String password1 = request.getParameter("password1");
+        String password2 = request.getParameter("password2");
 
+        // DBアクセスのためのクラスをインスタンス化
+        Dao dao = new Dao();
 
-	}
+        // パスワード強化チェック
+        if (!isPasswordStrong(password1)) {
+            // パスワードが強化基準を満たしていない場合、エラーメッセージを設定
+            request.setAttribute("errorMessage", "パスワードは8文字以上、小文字、大文字、数字、記号を含む必要があります。");
+            RequestDispatcher dispatcher = request.getRequestDispatcher("./WEB-INF/jsp/entryUser.jsp");
+            dispatcher.forward(request, response);
+            return; // 処理を終了
+        }
 
+        // ユーザ情報が問題ないかチェック
+        if (!id.isBlank() && dao.getUserById(id) == null && password1.equals(password2) && !password1.isBlank()) {
+            // 登録用のユーザオブジェクトを作成（Userはbeansパッケージに定義）
+            User userToEntry = new User(id, password1, name);
+            // ユーザをDBに登録
+            dao.insertUser(userToEntry);
+            // ログインページへリダイレクト
+            RequestDispatcher dispatcher = request.getRequestDispatcher("./WEB-INF/jsp/login.jsp");
+            dispatcher.forward(request, response);
+        } else {
+            // ユーザ情報に問題があった場合
+            RequestDispatcher dispatcher = request.getRequestDispatcher("./WEB-INF/jsp/entryUser.jsp");
+            dispatcher.forward(request, response);
+        }
+    }
 }
--- src/main/webapp/WEB-INF/jsp/entryUser.jsp
+++ src/main/webapp/WEB-INF/jsp/entryUser.jsp
@@ -1,5 +1,4 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 <html>
 <head>
@@ -8,16 +7,20 @@
 </head>
 <body>
 <label>Tiny BBS ユーザ登録</label>
-	<!--  EntryUserServletにデータを送信するformを宣言する. 登録処理なので，methodはpostを指定．
-	後はentryArticle.jspと同様なので，そちらのコメントを参考にするとよい．-->
-	
-	<form action="./EntryUserServlet" method="post">
-		<p><label>ログインID：<input type="text" name="id" size="40" maxlength="20"></label></p>
-		<p><label>名前：<input type="text" name="name" size="40" maxlength="20"></label></p>
-		<!-- typeをパスワードにすると，文字が目隠しされる．他にも，数値や日付，メールアドレスなどを入力するtypeが指定できるので，調べてみるとよい．-->
-		<p><label>パスワード：<input type="password" name="password1" size="40" maxlength="20"></label></p>
-		<p><label>確認用パスワード：<input type="password" name="password2" size="40" maxlength="20"></label></p>
-		<p><input type="submit" value="登録"></p>
-	</form>
+
+<%-- エラーメッセージを表示 --%>
+<% String errorMessage = (String) request.getAttribute("errorMessage"); %>
+<% if (errorMessage != null) { %>
+    <p style="color: red;"><%= errorMessage %></p>
+<% } %>
+
+<!-- EntryUserServletにデータを送信するform -->
+<form action="./EntryUserServlet" method="post">
+    <p><label>ログインID：<input type="text" name="id" size="40" maxlength="20"></label></p>
+    <p><label>名前：<input type="text" name="name" size="40" maxlength="20"></label></p>
+    <p><label>パスワード：<input type="password" name="password1" size="40" maxlength="20"></label></p>
+    <p><label>確認用パスワード：<input type="password" name="password2" size="40" maxlength="20"></label></p>
+    <p><input type="submit" value="登録"></p>
+</form>
 </body>
 </html>
