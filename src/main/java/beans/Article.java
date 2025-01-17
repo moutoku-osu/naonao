a1b6bd00bd420aad7dc6ad3258873b3fd38934e6
zamma ichiro <zamma ichiro@DESKTOP-BV5AC3O.lan>
2025-01-17 21:26:43 +0900

    3回目のコミット　（中山誠悟）

--- src/main/java/beans/Article.java
+++ src/main/java/beans/Article.java
@@ -3,70 +3,89 @@ package beans;
 import java.io.Serializable;
 import java.sql.Timestamp;
 
-//記事を表すクラス
-public class Article implements Serializable{ //implements Serializableが必要
-	private int id; //すべての記事で一意な番号
-	private String title; //タイトル
-	private String body; //本文
-	private String editorId; //著者のユーザId
-	private Timestamp entryDatetime; //登録日時
+// 記事を表すクラス
+public class Article implements Serializable { // implements Serializableが必要
+    private int id; // すべての記事で一意な番号
+    private String title; // タイトル
+    private String body; // 本文
+    private String editorId; // 著者のユーザId
+    private Timestamp entryDatetime; // 登録日時
 
-	//カラのコンストラクタが必要
-	public Article() {
-	}
+    // カラのコンストラクタが必要
+    public Article() {
+    }
 
-	public Article(int id, String title, String body, String editorId, Timestamp entryDatetime) {
-		super();
-		this.id = id;
-		this.title = title;
-		this.body = body;
-		this.editorId = editorId;
-		this.entryDatetime = entryDatetime;
-	}
+    public Article(int id, String title, String body, String editorId, Timestamp entryDatetime) {
+        super();
+        this.id = id;
+        this.title = title;
+        this.body = body;
+        this.editorId = editorId;
+        this.entryDatetime = entryDatetime;
+    }
 
-	//idと登録日時が確定していない場合用のコンストラクタ
-	public Article(String title, String body, String editorId) {
-		this(-1, title, body, editorId, null);
-	}
+    // idと登録日時が確定していない場合用のコンストラクタ
+    public Article(String title, String body, String editorId) {
+        this(-1, title, body, editorId, null);
+    }
 
-	public int getId() {
-		return id;
-	}
+    public int getId() {
+        return id;
+    }
 
-	public void setId(int id) {
-		this.id = id;
-	}
+    public void setId(int id) {
+        this.id = id;
+    }
 
-	public String getTitle() {
-		return title;
-	}
+    public String getTitle() {
+        return title;
+    }
 
-	public void setTitle(String title) {
-		this.title = title;
-	}
+    public void setTitle(String title) {
+        this.title = title;
+    }
 
-	public String getBody() {
-		return body;
-	}
+    public String getBody() {
+        return body;
+    }
 
-	public void setBody(String body) {
-		this.body = body;
-	}
+    public void setBody(String body) {
+        this.body = body;
+    }
 
-	public String getEditorId() {
-		return editorId;
-	}
+    public String getEditorId() {
+        return editorId;
+    }
 
-	public void setEditorId(String editorId) {
-		this.editorId = editorId;
-	}
+    public void setEditorId(String editorId) {
+        this.editorId = editorId;
+    }
 
-	public Timestamp getEntryDatetime() {
-		return entryDatetime;
-	}
+    public Timestamp getEntryDatetime() {
+        return entryDatetime;
+    }
 
-	public void setEntryDatetime(Timestamp entryDatetime) {
-		this.entryDatetime = entryDatetime;
-	}
+    public void setEntryDatetime(Timestamp entryDatetime) {
+        this.entryDatetime = entryDatetime;
+    }
 
+    // タイトルの文字数を取得
+    public int getTitleLength() {
+        return title != null ? title.length() : 0;
+    }
+
+    // 本文の文字数を取得
+    public int getBodyLength() {
+        return body != null ? body.length() : 0;
+    }
+
+    // 記事情報を文字列として返す
+    @Override
+    public String toString() {
+        return "記事ID: " + id + "\n"
+                + "タイトル: " + title + " (" + getTitleLength() + "文字)\n"
+                + "本文: " + body + " (" + getBodyLength() + "文字)\n"
+                + "著者ID: " + editorId + "\n"
+                + "登録日時: " + (entryDatetime != null ? entryDatetime.toString() : "未登録");
+    }
 }
--- src/main/webapp/WEB-INF/jsp/articleList.jsp
+++ src/main/webapp/WEB-INF/jsp/articleList.jsp
@@ -16,30 +16,32 @@
 <title>Article list page</title>
 </head>
 <body>
-	<label>記事一覧</label><br><br>
-	<!-- UpdateUserPageServletAnsにハイパーリンク．
-	ハイパーリンクによるアクセスは，GETアクセスとなり，サーブレットのdoGet()が呼ばれる． -->
-	<a href = ./UpdateUserPageServletAns>ユーザ情報更新</a><br><br>
-	<!-- EntryArticlePageServletにハイパーリンク．
-	ハイパーリンクによるアクセスは，GETアクセスとなり，サーブレットのdoGet()が呼ばれる． -->
-	<a href = ./EntryArticlePageServlet>新規記事登録</a><br><br>
+    <label>記事一覧</label><br><br>
+    <!-- UpdateUserPageServletAnsにハイパーリンク．
+    ハイパーリンクによるアクセスは，GETアクセスとなり，サーブレットのdoGet()が呼ばれる． -->
+    <a href="./UpdateUserPageServletAns">ユーザ情報更新</a><br><br>
+    <!-- EntryArticlePageServletにハイパーリンク．
+    ハイパーリンクによるアクセスは，GETアクセスとなり，サーブレットのdoGet()が呼ばれる． -->
+    <a href="./EntryArticlePageServlet">新規記事登録</a><br><br>
 
-	<!-- 直前のサーブレットでrequestにsetAttributeしておいたオブジェクトを取り出す．
-	普通に取り出すとObject型になってしまうので，適宜キャストして変数に受け取る． -->
-	<%List<Article> aList = (List<Article>)( request.getAttribute("articleList") );%>
-	
-	<!-- javaの文法とhtmlを組み合わせて，記事リストを表示 -->
-	<%for(Article a : aList){%>
-		<!-- <label><%=String.valueOf(a.getId() )%></label> <br> -->
-		<!-- %の直後に=を付けると，そのまま値を表示できる．代わりに，<%out.print(a.getTitle());%>のように書くことも可能 -->
-		<label>タイトル：<br><%=a.getTitle()%></label> <br>
-		<label>本文：<br><%=a.getBody()%></label> <br>
-		<label>登録者：<br><%=a.getEditorId()%></label> <br>
-		<!-- 日時を指定のフォーマットで表示するための命令 -->
-		<%SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");%>
-		<label>登録日時：<br><%=sdf.format( a.getEntryDatetime() )%></label>
-		<br>
-		<br>
-	<%} %>
+    <!-- 直前のサーブレットでrequestにsetAttributeしておいたオブジェクトを取り出す．
+    普通に取り出すとObject型になってしまうので，適宜キャストして変数に受け取る． -->
+    <%List<Article> aList = (List<Article>) (request.getAttribute("articleList"));%>
+    
+    <!-- javaの文法とhtmlを組み合わせて，記事リストを表示 -->
+    <%if (aList != null && !aList.isEmpty()) { %>
+        <%for (Article a : aList) { %>
+            <!-- 記事情報を表示 -->
+            <label>タイトル：<br><%= a.getTitle() %> (<%= a.getTitleLength() %>文字)</label> <br>
+            <label>本文：<br><%= a.getBody() %> (<%= a.getBodyLength() %>文字)</label> <br>
+            <label>登録者：<br><%= a.getEditorId() %></label> <br>
+            <!-- 日時を指定のフォーマットで表示するための命令 -->
+            <%SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");%>
+            <label>登録日時：<br><%= a.getEntryDatetime() != null ? sdf.format(a.getEntryDatetime()) : "未登録" %></label>
+            <br><br>
+        <%} %>
+    <%} else { %>
+        <p>現在、記事は登録されていません。</p>
+    <%} %>
 </body>
 </html>
